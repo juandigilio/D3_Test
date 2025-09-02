5b1f38fd119a5b2b497817ffefa5be2cbd7acf18
Juan Digilio <juanignaciodigilio@gmail.com>
2025-09-02 18:01:53 -0300

    Updated Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,16 +1,84 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Vector2 startPosition;
+    Vector2 direction;
+
+    float speed;
+    float lifeDistance;
+    int damage;
+    bool isDestroyable;
+    bool isActive = false;
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isActive) return;
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (Vector2.Distance(startPosition, transform.position) >= lifeDistance)
+        {
+            Deactivate();
+        }
+        else
+        {
+            Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+
+            if (screenPoint.x < 0 || screenPoint.x > 1 || screenPoint.y < 0 || screenPoint.y > 1)
+            {
+                Deactivate();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isActive) return;
+
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+
+                if (isDestroyable)
+                {
+                    Deactivate();
+                }
+            }
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
+
+    public void Activate(Vector2 startPosition, Vector2 direction, float speed, float lifeDistance, int damage, bool isDestroyable)
+    {
+        transform.position = startPosition;
+        this.startPosition = startPosition;
+        this.direction = direction.normalized;
+        this.speed = speed;
+        this.lifeDistance = lifeDistance;
+        this.damage = damage;
+        this.isDestroyable = isDestroyable;
+
+        isActive = true;
+        gameObject.SetActive(true);
+    }
+
+    private void Deactivate()
+    {
+        isActive = false;
+        gameObject.SetActive(false);
     }
 }
